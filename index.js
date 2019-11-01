2ce1a594ef4e37c17896192a71453da740b31554
Rob Hess <robwhess@gmail.com>
2019-11-01 16:13:22 -0700

    Add code from 11/1 lecture.

---
+++ index.js
@@ -0,0 +1,7 @@
+window.addEventListener('click', function (event) {
+  console.log("== window was clicked, event:", event);
+});
+
+// window.addEventListener('mousemove', function () {
+//   console.log("== mouse moved in window");
+// });
