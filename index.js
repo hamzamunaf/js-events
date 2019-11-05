8eee3d1b75625a199b9a67d5fc3abb8c7c8a56ea
Rob Hess <robwhess@gmail.com>
2019-11-04 16:14:01 -0800

    Add code from 11/4 lecture.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ window.addEventListener('click', function (event) {
 // window.addEventListener('mousemove', function () {
 //   console.log("== mouse moved in window");
 // });
+
+function handleBoxClick(event) {
+  console.log("== A box was clicked");
+  console.log("  - event.currentTarget:", event.currentTarget);
+  console.log("  - event.target:", event.target);
+  event.currentTarget.classList.toggle('highlighted');
+}
+
+var boxes = document.getElementsByClassName('box');
+for (var i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener('click', handleBoxClick);
+}
+
+var buttons = document.getElementsByClassName('in-box-button');
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function (event) {
+    console.log("== A button was clicked");
+    event.stopPropagation();
+  });
+}
+
+var links = document.getElementsByTagName('a');
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', function (event) {
+    console.log("== A link was clicked");
+    event.preventDefault();
+  });
+}
